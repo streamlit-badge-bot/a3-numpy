994031c397b6a8441f51bbfeccb5b632304f9f7a
Zeng Zixuan <zzx02gtx@gmail.com>
2020-10-23 01:18:58 +0800

    [Feature] Add two plots and some words

--- airbnb.py
+++ airbnb.py
@@ -4,6 +4,7 @@ import altair as alt
 import pydeck as pdk
 import numpy as np
 import geopandas as gp
+import plotly.express as px
 
 
 CITY = "San Francisco"
@@ -21,7 +22,6 @@ COLORS_G = {"green": 128, "blue": 0, "orange": 165, "gray": 128, "red": 0,}
 
 COLORS_B = {"green": 0, "blue": 255, "orange": 0, "gray": 128, "red": 0, }
 
-st.title("Let's analyze some Airbnb listings Data🏠")
 
 @st.cache
 def load_data():
@@ -50,22 +50,50 @@ def load_geojson_data():
 
 df_listing, df_review = load_data()
 
+
+st.title("Airbnb listings in San Francisco🏠")
+
+
+st.markdown('In this project, we are going to analyze the airbnb listing data in San Francisco. ✈️')
+
+st.markdown('Let\'s take a look at the raw data from [insideAirbnb](http://insideairbnb.com/) and its descriptive statstics.')
+
 if st.checkbox("Show Raw Data"):
     st.dataframe(df_listing.head())
+
+if st.checkbox('Show Data Statistics'):
+	st.dataframe(df_listing.describe())
 
 geodata = load_geojson_data()
 
 
-st.header("Filter Settings")
+st.header("Listing Locations")
+
+df_nbh = df_listing.groupby('neighbourhood_cleansed').mean()
+
+st.markdown('We are interested in the popular areas of SF, from the chart we could observe that *Seacliff* and *Maria* are with higher average price. If the trip is budgeted, we could look into other neighbourhoods like *Crocker Amazon*.')
+
+bar = px.bar(df_nbh['price'].sort_values(ascending=False).reset_index(), x="neighbourhood_cleansed", y='price', title="Average price of each neighbourhood in SF", color = 'neighbourhood_cleansed')
+bar.update_xaxes(title="Neighbourhood")
+bar.update_yaxes(title="Price")
+st.plotly_chart(bar)
+#print(df_nbh)
+
+stats_nbh = geodata.merge(df_nbh, left_on = 'neighbourhood', right_on = 'neighbourhood_cleansed')
+print(stats_nbh['price'])
 
 # midpoint = (np.average(df_listing['longitude']), np.average(df_listing['latitude']))
 
 stats = geodata.merge(df_listing, 'left', 'neighbourhood')
 
-# Sidebar
+
+
 room_types = list(df_listing['room_type'].unique())
 neighbourhoods = [CITY] + list(df_listing['neighbourhood_cleansed'].unique())
 
+st.markdown('Knowing the price distribution across the neighbourhoods, we are then going to explore more on data. The map visualization below gives a clearer insight of the listings in SF, the color indicates the room types and the size of circle indicates the price.\
+	By interacting with the widget you could explore more factors that affect the prices, try it out🙌')
+st.header("Map Visualization")
 room_types_multiselect = st.multiselect(
     label='Room types',
     options=room_types,
@@ -152,7 +180,7 @@ deck = pdk.Deck(
     		 data=filtered_listing,
     		 get_position='[longitude, latitude]',
     		 auto_highlight=True,
-    		 get_radius=30,          # Radius is given in meters
+    		 get_radius='price / 5 + 30',          # Radius is given in meters
     		 get_fill_color='[color_r, color_g, color_b, color_a]',  # Set an RGBA value for fill
     		 pickable=True,
     		 )
@@ -163,13 +191,30 @@ deck = pdk.Deck(
  )
 
 
-st.header("Map Visualization")
-
 st.pydeck_chart(deck)
+
+st.header("Availability")
+
+st.write("Then let's look at the availability of the rooms in SF. Here we define it of *HIGH* availability if its available days >= 60 in a year.")
+
+df_available = df_listing.groupby('availability').count().reset_index()
+pie = px.pie(df_available, values=df_available['availability_365'], names= df_available['availability'], title="Pie Chart of Availability", color = df_available['availability'])
+
+st.plotly_chart(pie)
+
+st.write("An observation is that the room is either fully available or not available.")
+
+his = px.histogram(df_listing, x="availability_365", nbins=100, title="Availability distribution")
+his.update_xaxes(title="Availability Days")
+his.update_yaxes(title="Listings")
+st.plotly_chart(his)
 
 st.header("Scatter Visualization of Price")
 
-column = ["neighbourhood_cleansed","bedrooms","beds","review_scores_rating"]
+
+st.markdown('In addition, we want to learn how the number of bedrooms, ratings and capacity affect the price.')
+
+column = ["neighbourhood_cleansed","bedrooms","beds","review_scores_rating", "accommodates"]
 x_axis = st.selectbox('X Axis',column)
 
 color_list = ["room_type", "neighbourhood_cleansed", "bedrooms", "review_scores_rating"]
@@ -184,6 +229,7 @@ scatter = alt.Chart(filtered_listing).mark_point().encode(
 st.write(scatter)
 
 st.header("Price Estimation")
+st.markdown('Finally, before we actually book an order, let\'s estimate how much we are going to pay.')
 st.write("Please provide the room type, location and the number of bedrooms")
 est_list = ["room_type", "neighbourhood_cleansed", "bedrooms"]
 est_room_type = st.selectbox('Room Type', df_listing['room_type'].unique())
@@ -202,3 +248,40 @@ if est_price==0:
     st.write("Fail to estimate the price.")
 else:
     st.write("The estimation price is: $"+str(est_price)+" per night.")
+
+# tooltipnbh_html = """
+# <h3>{price}</h3>
+# """
+
+# deck_nbh = pdk.Deck(
+# 	map_style='mapbox://styles/mapbox/light-v10',
+# 	initial_view_state=pdk.ViewState(
+# 		latitude=midpoint[1],
+# 		longitude=midpoint[0],
+# 		zoom=11.5,
+# 		min_zoom=10,
+# 		max_zoom=13,
+# 		pitch=40.5,
+# 		bearing=-27.36
+#      ),
+# 	layers=[
+#          pdk.Layer(
+#              'GeoJsonLayer',
+#              data=stats_nbh,
+#              get_position='[longitude, latitude]',
+#              get_fill_color='[color_r, color_g, color_b, color_a]',
+#              get_elevation='price',
+#              elevation_scale = 20,
+#              get_line_color=[0,0,0],
+#              get_line_width=10,
+#              opacity=1.0,
+#              stroked=False,
+#              filled=True,
+#              extruded=True,
+#              pickable=True,
+#              #wireframe=True,
+#          ),
+#     ],
+#     tooltip=tooltipnbh_html,
+# )
+# st.pydeck_chart(deck_nbh)
